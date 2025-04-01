a6ac4c7b11e72598272007e8ea5aa1655f3666bc
David Henrique <dh210605@gmail.com>
2025-03-31 22:28:23 -0300

    [feature]: informação aleatoria API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const Home = () => {
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/info")
+      .then(response => setInfo(response.data))
+      .catch(error => console.error("Erro ao buscar info:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Salve marcio, tmj fera
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Informações da API</h1>
+      {info ? (
+        <div>
+          <p>Status: {info.status}</p>
+          <img src={info.message} alt="Imagem aleatória" style={{ width: "300px" }} />
+        </div>
+      ) : (
+        <p>Carregando...</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default Home;
